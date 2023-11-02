36d05ca117d0afcb0a828a08d8ff8f0f5cd2a52b
Breton Rémy <remy.breton@epitech.eu>
2023-11-02 13:40:25 +0100

    :sparkles: Can now draw texture on square

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,14 @@ extern crate sdl2;
 mod window;
 mod square;
 
+use sdl2::rect::Rect;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::video::Window;
 use sdl2::render::Canvas;
 use sdl2::pixels::Color;
+use sdl2::render::TextureCreator;
+use sdl2::image::LoadTexture;
 use square::create_squares;
 
 fn main() {
@@ -22,6 +25,12 @@ fn main() {
     let mut event_pump: sdl2::EventPump = game.1;
     let square = square::Square::new(0, 0, Color::RGB(255, 0, 0));
     let vect: Vec<Vec<square::Square>> = create_squares(0, 0, Color::RGB(255, 0, 0), 5, 7);
+    let _image_context = sdl2::image::init(sdl2::image::InitFlag::JPG).unwrap();
+    let texture_creator: TextureCreator<_> = canvas.texture_creator();
+
+    let texture = texture_creator
+        .load_texture("./assets/grass.jpg")
+        .expect("Failed to load texture");
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -40,7 +49,8 @@ fn main() {
         // logic here
         for row in &vect {
             for block in row {
-                block.draw(&mut canvas);
+                let dest_rect = Rect::new(block.rect.x, block.rect.y, block.rect.width(), block.rect.height());
+                canvas.copy(&texture, None, dest_rect).expect("Échec du rendu de la texture sur le carré");
             }
         }
 
