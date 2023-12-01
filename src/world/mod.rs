d02dd8cb29e8121bd205fd3cccd4fe65c525a1bf
Breton Rémy <remy.breton@epitech.eu>
2023-12-01 13:59:25 +0100

    :bug: Fix camera breaking block placement

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -8,7 +8,7 @@
 pub mod square;
 pub mod script;
 use self::square::Square;
-use crate::GLOBAL_VECT;
+use crate::{GLOBAL_VECT, GLOBAL_CAMERA};
 
 const CUBE_SIZE: i32 = 50;
 
@@ -28,10 +28,11 @@ pub fn init_world<'a>(size: u32) -> Vec<Vec<Option<Square>>> {
 
 pub fn create_block<'a>(x: i32, y: i32) {
     let mut vect = GLOBAL_VECT.lock().unwrap();
+    let camera = GLOBAL_CAMERA.lock().unwrap();
 
-    if let Some(val) = vect.get_mut((y / CUBE_SIZE) as usize) {
-        if let Some(bloc) = val.get_mut((x / CUBE_SIZE) as usize) {
-            *bloc = Some(Square::new(x, y));
+    if let Some(val) = vect.get_mut((y / CUBE_SIZE + (camera.y * -1) / CUBE_SIZE) as usize) {
+        if let Some(bloc) = val.get_mut((x / CUBE_SIZE + (camera.x * -1) / CUBE_SIZE) as usize) {
+            *bloc = Some(Square::new(x + ((camera.x * -1) / CUBE_SIZE) * CUBE_SIZE, y + ((camera.y * -1) / CUBE_SIZE) * CUBE_SIZE));
         }
     }
 }
